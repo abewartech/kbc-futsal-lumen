d535828ab804549f7fb2ba9134e4384b4fcb80a6
abe_wartech <yusufarifin89@gmail.com>
2020-04-30 11:30:15 +0700

    AB : Routes & Controllers

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,19 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->post("/register", "AuthController@register");
 $router->post("/login", "AuthController@login");
+
+$router->post("/adduser", "UsersController@adduser");
+$router->post("/getallusers", "UsersController@getallusers");
+$router->post("/getuser/{id}", "UsersController@getuser");
+$router->post("/changepassword/{id}", "UsersController@changepassword");
+$router->post("/deleteuser/{id}", "UsersController@deleteuser");
+
+$router->post("/addbooking", "BookingController@addbooking");
+$router->post("/upload", "BookingController@upload");
+$router->post("/getallbooking", "BookingController@getallbooking");
+$router->post("/getbooking/{id}", "BookingController@getbooking");
+$router->post("/editbooking/{id}", "BookingController@editbooking");
+$router->post("/deletebooking/{id}", "BookingController@deletebooking");
+$router->post("/completebooking/{id}", "BookingController@completebooking");
+$router->post("/getallcompletebooking", "BookingController@getallcompletebooking");
